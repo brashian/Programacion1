15bffb9fc08d0f426b2ea061ed0483ae56bcf81e
brashian <bsandovala12@gmail.com>
2022-03-28 16:48:23 -0300

    Ejercicio 3

---
+++ ejercicio_3/index.js
@@ -0,0 +1,17 @@
+
+
+function calcular_potencia()
+{
+    const tension = document.getElementById("inp_tension").value
+    const corriente = document.getElementById("inp_corriente").value
+
+    const potencia = tension * corriente;
+
+   return potencia
+}
+
+function dar_resultado()
+{
+    const respuesta = calcular_potencia()
+    alert(respuesta)
+}
--- variables_funciones/index.js
+++ variables_funciones/index.js
@@ -1,5 +1,36 @@
 //funcion clasica
 function mostrar (){
-    alert('probando')
+    //forma uno
+    //const nombres = document.querySelector("#inp_nombre").value
+
+    //forma dos
+    const nombres = document.getElementById("inp_nombre").value
+
+
+
+
+    //sin template string
+    //const mensaje = 'Nombre y Apellido '+nombres
+ 
+
+    //con template string
+    const mensaje = `Nombre y apellido: ${nombres} - DNI: 45471450`
+
+    alert(mensaje)
+}
+
+//funcion con retorno, la retorna al que la invoca, llama.
+function mifunction_con_retorno ()
+{
+    const ejemplo = 'Probando funcion con retorno'
+    //retorna una repuesta
+    return ejemplo
+}
+
+function mostrar_respuest()
+{
+// se invoca la funcion.
+    const respuesta =      mifunction_con_retorno()
+    alert(respuesta)
 }
 

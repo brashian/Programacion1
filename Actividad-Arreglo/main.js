73b9274f3669f2ffb3eceea256402181dd8220fb
brashian <bsandovala12@gmail.com>
2022-05-03 14:18:26 -0300

    final

--- Actividad-Arreglo/main.js
+++ Actividad-Arreglo/main.js
@@ -5,7 +5,25 @@ c) El tercer elemento del arreglo
 2. Crear una función que recorra el arreglo y lo imprima en un console.log(). Hacer uso de
 forEach para dicho recorrido.
  */
-let lenguajes = ["Python","JavaScript","Csharp","C++","Java","PHP","Ruby"]
+var lenguajes = ["Python","JavaScript","Csharp","C++","Java","PHP","Ruby"]
+
+
+/*3. Agregar en lenguaje ‘Go’ al final del arreglo.
+4. Eliminar el primer lenguaje del arreglo
+5. Agregar al inicio del arreglo el lenguaje ‘Kotlin’
+6. Eliminar el lenguaje Java y Php mediante el uso de splice(posición,elementos)
+7. Finalmente imprimir el arreglo en un console.log() */
+
+lenguajes.push('Go')
+console.log(` Se agrega el elemento GO ${lenguajes} al final del arreglo`)
+lenguajes.shift()
+console.log(` Se elimina el primer elemento ${lenguajes}`
+)
+lenguajes.unshift('Kotlin')
+console.log(`Se agrega un elemento al inicio del array ${lenguajes}`)
+let index=lenguajes.indexOf("Java")
+lenguajes.splice(index,2)
+console.log(`Se eliminan Java Y PHP con index y el uso de splice = ${lenguajes}`)
 
 const arreglo = () =>{
     let final = lenguajes [lenguajes.length-1]
@@ -17,6 +35,8 @@ const arreglo = () =>{
         `
 
 }
+const boton = document.getElementById('btn_mostrar')
+boton.addEventListener("click", arreglo)
 
 const recorrer = () =>{
 
@@ -28,13 +48,4 @@ const recorrer = () =>{
     
     })
 }
-
-/*3. Agregar en lenguaje ‘Go’ al final del arreglo.
-4. Eliminar el primer lenguaje del arreglo
-5. Agregar al inicio del arreglo el lenguaje ‘Kotlin’
-6. Eliminar el lenguaje Java y Php mediante el uso de splice(posición,elementos)
-7. Finalmente imprimir el arreglo en un console.log() */
-
-lenguajes.push('Go')
-console.log(` Se agrega lenguaje ${lenguajes} al final del arreglo`)
-lenguajes.shift
+boton.addEventListener("click", recorrer)

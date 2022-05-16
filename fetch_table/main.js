35b17a9476f36c9b8b691067c7899b783413ef4d
brashian <bsandovala12@gmail.com>
2022-05-16 17:06:51 -0300

    mas uso de objetos

---
+++ fetch_table/main.js
@@ -0,0 +1,26 @@
+const consumir_datos = async() =>{
+
+    const datos_recibidos = await fetch ('https://jsonplaceholder.typicode.com/users')
+    const datos = await datos_recibidos.json()
+
+
+    let lista_usuarios = []
+    var contador = 0;
+    datos.forEach(usuarios => {
+        
+        contador= contador+1
+        let fila = `<tr>
+                        <th>${contador}</th>
+                        <td>${usuarios.name}</td>
+                        <td>${usuarios.email}</td>
+                        <td>${usuarios.website}</td>
+                    </tr>`
+       
+        lista_usuarios.push(fila)
+        contador
+    });
+
+    document.getElementById('tbl_body').innerHTML=lista_usuarios.join('')
+} 
+
+consumir_datos()

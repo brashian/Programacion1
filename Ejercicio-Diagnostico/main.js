f886ee277ac9ae1ebf2e39e859076495ad80f908
brashian <bsandovala12@gmail.com>
2022-06-28 14:31:12 -0300

    diagnostico

--- Ejercicio-Diagnostico/main.js
+++ Ejercicio-Diagnostico/main.js
@@ -1,7 +1,7 @@
 let numero = [1,2,3,4,5,6,7,8,9,10]
-let par = []
+
 const mostrar = () =>{
-  
+    let par = []
     numero.forEach(element => {
         if (element%2==0){
             par.push(element)

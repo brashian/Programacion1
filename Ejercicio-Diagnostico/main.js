1fd4a26458efc2c7d42f96d2cd192ee792316fb0
brashian <bsandovala12@gmail.com>
2022-06-28 14:30:06 -0300

    Diagnostico

---
+++ Ejercicio-Diagnostico/main.js
@@ -0,0 +1,35 @@
+let numero = [1,2,3,4,5,6,7,8,9,10]
+let par = []
+const mostrar = () =>{
+  
+    numero.forEach(element => {
+        if (element%2==0){
+            par.push(element)
+        }else{
+            
+        }
+
+
+
+    });
+    console.log(par)
+}
+mostrar()
+
+const respuesta = () => {
+    let arreglo = []
+for (let valor  of numero) {
+    
+    if(valor>2&& valor<9){
+        arreglo.push(valor)
+    
+    }else{
+        
+    }
+
+
+
+}
+    console.log(arreglo)
+}
+respuesta()
